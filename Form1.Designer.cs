dca0d03ada115fb8c741beb13713b650a2c431d1
iamakshay <gpbijnor1@gmail.com>
2021-12-28 13:43:14 +0530

    Machine Key Generator

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace TestMachineKey
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.txtMachineKey = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radNet1 = new System.Windows.Forms.RadioButton();
+			this.radNet2 = new System.Windows.Forms.RadioButton();
+			this.btnGenerate = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+			this.splitContainer1.IsSplitterFixed = true;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.btnGenerate);
+			this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.txtMachineKey);
+			this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.splitContainer1.Size = new System.Drawing.Size(573, 578);
+			this.splitContainer1.SplitterDistance = 82;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// txtMachineKey
+			// 
+			this.txtMachineKey.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtMachineKey.Location = new System.Drawing.Point(0, 0);
+			this.txtMachineKey.Multiline = true;
+			this.txtMachineKey.Name = "txtMachineKey";
+			this.txtMachineKey.ReadOnly = true;
+			this.txtMachineKey.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtMachineKey.Size = new System.Drawing.Size(573, 492);
+			this.txtMachineKey.TabIndex = 0;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radNet2);
+			this.groupBox1.Controls.Add(this.radNet1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(300, 52);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Target version";
+			// 
+			// radNet1
+			// 
+			this.radNet1.AutoSize = true;
+			this.radNet1.Location = new System.Drawing.Point(6, 19);
+			this.radNet1.Name = "radNet1";
+			this.radNet1.Size = new System.Drawing.Size(105, 17);
+			this.radNet1.TabIndex = 0;
+			this.radNet1.Text = ".NET version &1.1";
+			this.radNet1.UseVisualStyleBackColor = true;
+			// 
+			// radNet2
+			// 
+			this.radNet2.AutoSize = true;
+			this.radNet2.Checked = true;
+			this.radNet2.Location = new System.Drawing.Point(129, 19);
+			this.radNet2.Name = "radNet2";
+			this.radNet2.Size = new System.Drawing.Size(149, 17);
+			this.radNet2.TabIndex = 1;
+			this.radNet2.TabStop = true;
+			this.radNet2.Text = ".NET version &2.0 and later";
+			this.radNet2.UseVisualStyleBackColor = true;
+			// 
+			// btnGenerate
+			// 
+			this.btnGenerate.Location = new System.Drawing.Point(318, 28);
+			this.btnGenerate.Name = "btnGenerate";
+			this.btnGenerate.Size = new System.Drawing.Size(144, 23);
+			this.btnGenerate.TabIndex = 1;
+			this.btnGenerate.Text = "&Generate Keys";
+			this.btnGenerate.UseVisualStyleBackColor = true;
+			this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(573, 578);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "Form1";
+			this.Text = "MachineKey Generator";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TextBox txtMachineKey;
+		private System.Windows.Forms.Button btnGenerate;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton radNet2;
+		private System.Windows.Forms.RadioButton radNet1;
+
+	}
+}
+
---
+++ MachineKey.cs
@@ -0,0 +1,77 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+// This source code and all associated files and resources are copyrighted by
+// the author(s). This source code and all associated files and resources may
+// be used as long as they are used according to the terms and conditions set
+// forth in The Code Project Open License (CPOL).
+//
+// Copyright (c) 2011 Jonathan Wood
+//
+
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlackBeltCoder
+{
+	/// <summary>
+	/// Version arguments to MachineKey.Generate() method.
+	/// </summary>
+	public enum MachineKeyVersion
+	{
+		/// <summary>
+		/// .NET version 1.1.
+		/// </summary>
+		Net1,
+
+		/// <summary>
+		/// .NET version 2.0 and up.
+		/// </summary>
+		Net2,
+	}
+
+	public class MachineKey
+	{
+		/// <summary>
+		/// Generates the contents of a machineKey element suitable for use in
+		/// an ASP.NET web.config file.
+		/// </summary>
+		/// <param name="version">Indicates if keys should be generated for
+		/// ASP.NET 1.1 or 2.0 and later.</param>
+		public static string Generate(MachineKeyVersion version)
+		{
+			// Generate keys
+			string validationKey = GenerateKey(64);
+			string decryptionKey;
+			if (version == MachineKeyVersion.Net1)
+				decryptionKey = GenerateKey(24);
+			else
+				decryptionKey = GenerateKey(32);
+
+			// Construct <machineKey> tag
+			StringBuilder builder = new StringBuilder();
+			builder.Append("<machineKey");
+			builder.AppendFormat(" validationKey=\"{0}\"", validationKey);
+			builder.AppendFormat(" decryptionKey=\"{0}\"", decryptionKey);
+			builder.Append(" validation=\"SHA1\"");
+			if (version == MachineKeyVersion.Net2)
+				builder.Append(" decryption=\"AES\"");
+			builder.Append(" />");
+			return builder.ToString();
+		}
+
+		/// <summary>
+		/// Generates a string of random hex digits of the specified
+		/// number of bytes.
+		/// </summary>
+		/// <param name="length">Number of bytes to generate</param>
+		protected static string GenerateKey(int length)
+		{
+			RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+			byte[] buff = new byte[length];
+			rngCsp.GetBytes(buff);
+			StringBuilder sb = new StringBuilder(buff.Length * 2);
+			for (int i = 0; i < buff.Length; i++)
+				sb.Append(string.Format("{0:X2}", buff[i]));
+			return sb.ToString();
+		}
+	}
+}
